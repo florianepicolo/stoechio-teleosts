65527f0cc79366d95dfa9b58acc0a886ea3a7905
Picolo Floriane <florianepicolo@live.fr>
2023-01-17 08:34:41 +0100

    différenciation 3WGD et 4WGD

--- 6_statistics-chi2.R
+++ 6_statistics-chi2.R
@@ -1,25 +1,32 @@
-###!/usr/bin/Rscript --slave
+#!/usr/bin/Rscript --vanilla
 
 
 library(dplyr)
 library(vroom)
 
-setwd("~/Desktop/Pathways/prog_2023")
+setwd("~/Desktop/Pathways/stoechio-teleosts")
 
 
-f.stats.teleosts <- "p-stats-chi2-teleosteens.csv"
+# f.stats.teleosts <- "p-min-stats-chi2-teleosteens.csv"
+f.stats.teleosts <- "p-max-stats-chi2-teleosteens.csv"
 d.stats.teleosts <- vroom(f.stats.teleosts)
 
 d.stats.teleosts <- d.stats.teleosts %>% group_by(species) %>%
   mutate(nb_g_total = sum(nb_g_single, nb_g_dupli, nb_g_tripli),
          nb_p_total = sum(nb_p_single, nb_p_dupli, nb_p_tripli),
-         chi2_pval = chisq.test(x=c(nb_p_single, nb_p_dupli + nb_p_tripli), 
-                              p=c(nb_g_single/nb_g_total, nb_g_dupli/nb_g_total + nb_g_tripli/nb_g_total))$p.value) %>%
-  ungroup() %>% mutate(chi2_pvalBH = p.adjust(chi2_pval, method = "BH", n = length(chi2_pval))) %>%
+         d_chi2_pval = chisq.test(x=c(nb_p_single + nb_p_tripli, nb_p_dupli), 
+                              p=c(nb_g_single/nb_g_total + nb_g_tripli/nb_g_total, nb_g_dupli/nb_g_total), correct=FALSE)$p.value,
+         t_chi2_pval = chisq.test(x=c(nb_p_single + nb_p_dupli,  nb_p_tripli), 
+                                  p=c(nb_g_single/nb_g_total + nb_g_dupli/nb_g_total, nb_g_tripli/nb_g_total), correct=FALSE)$p.value) %>%
+  ungroup() %>% mutate(d_chi2_pvalBH = p.adjust(d_chi2_pval, method = "BH", n = length(d_chi2_pval)),
+                       t_chi2_pvalBH = p.adjust(t_chi2_pval, method = "BH", n = length(t_chi2_pval))) %>%
   group_by(species) %>%
-  mutate(hypergeom_pval = phyper((nb_p_tripli + nb_p_dupli - 1), nb_g_tripli + nb_g_dupli,
-                            (nb_g_total - nb_g_tripli - nb_g_dupli), nb_p_total, lower.tail = FALSE )) %>%
-  ungroup() %>% mutate(hypergeom_pvalBH = p.adjust(hypergeom_pval, method = "BH", n = length(hypergeom_pval))) %>%
+  mutate(d_hypergeom_pval = phyper((nb_p_dupli - 1), nb_g_dupli,
+                            (nb_g_total - nb_g_dupli), nb_p_total, lower.tail = FALSE),
+         t_hypergeom_pval =  phyper((nb_p_tripli - 1), nb_g_tripli,
+                                    (nb_g_total - nb_g_tripli), nb_p_total, lower.tail = FALSE)) %>%
+  ungroup() %>% mutate(d_hypergeom_pvalBH = p.adjust(d_hypergeom_pval, method = "BH", n = length(d_hypergeom_pval)),
+                       t_hypergeom_pvalBH = p.adjust(t_hypergeom_pval, method = "BH", n = length(t_hypergeom_pval))) %>%
   group_by(species) %>%
   mutate(fc_single = (nb_p_single/nb_p_total)/(nb_g_single/nb_g_total),
          fc_dupli = (nb_p_dupli/nb_p_total)/(nb_g_dupli/nb_g_total),
@@ -27,5 +34,3 @@ d.stats.teleosts <- d.stats.teleosts %>% group_by(species) %>%
 
 do.stats.teleosts <- d.stats.teleosts %>% ungroup 
 sortie <- write.table(do.stats.teleosts, file=f.stats.teleosts, row.names = FALSE, col.names = TRUE, sep = ";", dec = ",")
-
-
